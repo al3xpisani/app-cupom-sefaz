bedd7ce62cecbc7751a4057acbf80829d7dcd4b7
Jimens <jimens@cesar.org.br>
2023-03-19 12:10:11 -0300

    Implementacao com Firebase

--- App.js
+++ App.js
@@ -1,20 +1,171 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native';
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import { BlurView } from 'expo-blur';
+import {initializeApp} from 'firebase/app';
+import { firebaseConfig } from './config/firebase-config';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const cesarLogo = 'https://www.cesar.org.br/image/layout_set_logo?img_id=1086110&t=1673865791645';
+
+function HomeScreen() {
+
+  const navigation = useNavigation();
+
+  const handleScan = () => {
+    navigation.navigate('Scan');
+  }
+
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela Principal</Text>
+      <Button title='Scan' onPress={handleScan}></Button>
+    </View>
+  );
+}
+
+function ScanScreen() {
+
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Scan QRCode</Text>
+    </View>
+  );
+}
+
+function LoginScreen()  {
+
+  const [email, setEmail] = React.useState(null);
+  const [password, setPassword] = React.useState(null);
+  const navigation = useNavigation();
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert('Conta Criada com Sucesso');
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log('Error' + error)
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log('Logged In');
+      const user = userCredential.user;
+      navigation.navigate('Home');
+    })
+    .catch((error) => {
+      console.log('Error' + error)
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={{width: '100%', height: '100%', position: 'absolute', backgroundColor: '#52D2B1'}}>
+        <Text style={{fontSize: 50, fontWeight: 'bold', color: 'white', textAlign: 'center', marginTop: 100}}>
+          Zero Queue
+        </Text>
+      </View>
+
+      <ScrollView contentContainerStyle={{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <BlurView intensity={100}>
+          <View style={styles.login}>
+            <View>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Email</Text>
+              <TextInput onChangeText={(text) => setEmail(text)} style={styles.input} placeholder="Email"/>
+            </View>
+            <View>
+              <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
+              <TextInput secureTextEntry={true} onChangeText={(text) => setPassword(text)} style={styles.input} placeholder="Senha" />
+            </View>
+            <View style={{flexDirection: 'column', flex:1, justifyContent: 'space-between'}}>
+              <TouchableOpacity onPress={handleLogin} style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text> 
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, {backgroundColor: '#6792F098'}]}>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Criar Conta</Text> 
+              </TouchableOpacity>
+            </View>
+          </View>
+        </BlurView>
+      </ScrollView>
+      <View style={{position: 'absolute', bottom: 0, width: '100%', height: 100, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Desenvolvido por CESAR</Text>  
+        <Image source={{cesarLogo}} style={StyleSheet.absoluteFill} />
+      </View>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Scan" component={ScanScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#52D2B1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    padding: 10
+  }, 
+  login: {
+    width: 350,
+    height: 300,
+    borderColor: '#6792F098',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',  
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20,
+  }
 });
---
+++ config/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDbSaeWEMbNuzOrMMu9qJNORWedgjZb5OY",
+  authDomain: "zeroqueue-8519f.firebaseapp.com",
+  databaseURL: "https://zeroqueue-8519f-default-rtdb.firebaseio.com",
+  projectId: "zeroqueue-8519f",
+  storageBucket: "zeroqueue-8519f.appspot.com",
+  messagingSenderId: "542005439200",
+  appId: "1:542005439200:web:cd11e54ff432a63abb7e85",
+  measurementId: "G-ERHBQZWL2T"
+};
