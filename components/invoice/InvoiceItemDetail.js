7da8fa5df61b4a175597376bd10368851bb520b2
al3xpisani <alexandre.pisani.ant@gmail.com>
2023-03-22 11:54:49 -0300

    fix bugs

--- components/invoice/InvoiceItemDetail.js
+++ components/invoice/InvoiceItemDetail.js
@@ -64,85 +64,106 @@ const InvoiceItemDetail = ({ route }) => {
   const { endereco, numero, bairro, cidade, uf } = details.emitente;
   const address = `${endereco}, ${numero}, ${bairro} - ${cidade} ${uf || ""}`;
 
-  return (
-    <View>
-      <View style={{ padding: 20 }}>
-        <Text style={{ marginBottom: 10, fontSize: 16, fontWeight: 300 }}>
-          EMISSOR
-        </Text>
+  const RenderInvoiceBody = () => {
+    return (
+      <View>
+        <View style={{ padding: 20 }}>
+          <Text style={{ marginBottom: 10, fontSize: 16, fontWeight: 300 }}>
+            EMISSOR
+          </Text>
 
-        <View style={{ diplay: "flex", flexDirection: "row", gap: 15 }}>
-          <Image
-            source={require("../../assets/images/ic_loja.png")}
-            style={{ width: 50, height: 50 }}
-          />
-          <View>
-            <Text style={{ fontSize: 21, fontWeight: 700 }}>
-              {details.emitente.razao_social}
-            </Text>
-            <Text>CNPJ {details.emitente.cnpj}</Text>
-            <Text>{details.emitente.razao_social}</Text>
+          <View style={{ diplay: "flex", flexDirection: "row", gap: 15 }}>
+            <Image
+              source={require("../../assets/images/ic_loja.png")}
+              style={{ width: 50, height: 50 }}
+            />
+            <View>
+              <Text style={{ fontSize: 21, fontWeight: 700 }}>
+                {details.emitente.razao_social}
+              </Text>
+              <Text>CNPJ {details.emitente.cnpj}</Text>
+              <Text>{details.emitente.razao_social}</Text>
+            </View>
           </View>
+
+          <Text style={{ fontWeight: 300 }}>IE</Text>
+          <Text>0000001111111</Text>
+
+          <Text style={{ fontWeight: 300 }}>Endereço</Text>
+          <Text>{address}</Text>
         </View>
 
-        <Text style={{ fontWeight: 300 }}>IE</Text>
-        <Text>0000001111111</Text>
-
-        <Text style={{ fontWeight: 300 }}>Endereço</Text>
-        <Text>{address}</Text>
-      </View>
-
-      <View
-        style={{
-          height: 2,
-          width: "100%",
-          backgroundColor: "lightgrey",
-        }}
-      />
-
-      <View style={{ padding: 20 }}>
-        <Text style={{ fontSize: 16, fontWeight: 300, marginBottom: 10 }}>
-          CONSUMIDOR
-        </Text>
-        <Text>Nome</Text>
-        <Text>{details.consumidor.nome || "-"}</Text>
-        <Text>CPF</Text>
-        <Text>{details.consumidor.cpf || "-"}</Text>
-      </View>
-
-      <View
-        style={{
-          height: 2,
-          width: "100%",
-          backgroundColor: "lightgrey",
-        }}
-      />
-
-      <View style={{ padding: 20 }}>
         <View
           style={{
-            diplay: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: 10,
+            height: 2,
+            width: "100%",
+            backgroundColor: "lightgrey",
           }}
-        >
-          <Text style={{ fontSize: 16, fontWeight: 300 }}>COMPRA</Text>
-          <Text style={{ fontWeight: 700 }}>{details.data_emissao}</Text>
-        </View>
-
-        <VirtualizedList
-          initialNumToRender={4}
-          renderItem={renderItem}
-          keyExtractor={keyExtractor}
-          getItemCount={getItemCount}
-          getItem={getItem}
-          data={products}
-          ItemSeparatorComponent={ItemSeparator}
         />
 
-        <Text style={{ fontSize: 18 }}>VALOR TOTAL</Text>
-        <Text style={{ fontSize: 21, fontWeight: 700 }}>
+        <View style={{ padding: 20 }}>
+          <Text style={{ fontSize: 16, fontWeight: 300, marginBottom: 10 }}>
+            CONSUMIDOR
+          </Text>
+          <Text>Nome</Text>
+          <Text>{details.consumidor.nome || "-"}</Text>
+          <Text>CPF</Text>
+          <Text>{details.consumidor.cpf || "-"}</Text>
+        </View>
+
+        <View
+          style={{
+            height: 2,
+            width: "100%",
+            backgroundColor: "lightgrey",
+          }}
+        />
+
+        <View style={{ padding: 20 }}>
+          <View
+            style={{
+              diplay: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: 10,
+            }}
+          >
+            <Text style={{ fontSize: 16, fontWeight: 300 }}>COMPRA</Text>
+            <Text style={{ fontWeight: 700 }}>{details.data_emissao}</Text>
+          </View>
+
+          <VirtualizedList
+            style={{ height: "40%" }}
+            initialNumToRender={4}
+            renderItem={renderItem}
+            keyExtractor={keyExtractor}
+            getItemCount={getItemCount}
+            getItem={getItem}
+            data={products}
+            ItemSeparatorComponent={ItemSeparator}
+          />
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 10, position: "relative" }}>
+        <RenderInvoiceBody />
+      </View>
+      <View
+        style={{
+          position: "sticky",
+          alignItems: "flex-end",
+          flex: 1,
+          padding: 20,
+          borderWidth: 1,
+          backgroundColor: "#540d6e",
+        }}
+      >
+        <Text style={{ fontSize: 18, color: "#ffffff" }}>VALOR TOTAL</Text>
+        <Text style={{ fontSize: 21, fontWeight: 700, color: "#ffffff" }}>
           R$ {details.total}
         </Text>
       </View>
