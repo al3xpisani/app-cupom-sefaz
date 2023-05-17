2b423b277e2b6ba11bb37b05bfb4af48bee51e61
al3x <alexandre.pisani.ant@gmail.com>
2023-05-17 13:21:40 -0300

    Update nfe-credentials.js

--- config/nfe-credentials.js
+++ config/nfe-credentials.js
@@ -1,5 +1,5 @@
 export const nfeCredentials = {
-    consumerKey: "EccuqWQoZNojrx2wKayCLMkxDEMgOwZR",
-    consumerSecret: "KphuoqHWVw0QGTPbwbBq0WPqwcdRsgVyEenZcRjOCziQbhTo",
-    xToken: "c3b1e07b-4d3a-492e-92a1-375ba47070fb"
+    consumerKey: "xx",
+    consumerSecret: "xxx",
+    xToken: "xxxx"
 }
