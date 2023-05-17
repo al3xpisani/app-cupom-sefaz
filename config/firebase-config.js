37e35237c47c9698efcbc7c8b6d26f18544f36e4
al3x <alexandre.pisani.ant@gmail.com>
2023-05-17 13:21:27 -0300

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -3,13 +3,13 @@ import {getFirestore, collection, addDoc} from 'firebase/firestore';
 import { ref } from 'firebase/database'
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAuf7ohDCRnu_CU50dOF3PH_7HaIFfvfvk",
-  authDomain: "zeroqueue-30894.firebaseapp.com",
-  projectId: "zeroqueue-30894",
-  storageBucket: "zeroqueue-30894.appspot.com",
-  messagingSenderId: "178763142648",
-  appId: "1:178763142648:web:a65ec8c44bcb4dab29720d",
-  measurementId: "G-7EGLC294L6"
+  apiKey: "xxxxx",
+  authDomain: "xxxx.com",
+  projectId: "xxxx",
+  storageBucket: "xxxx.com",
+  messagingSenderId: "xxxx",
+  appId: "xxxxx",
+  measurementId: "xxxx"
 };
 
 const app = initializeApp(firebaseConfig);
